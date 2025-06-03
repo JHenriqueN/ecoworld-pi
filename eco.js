e929bee2e75abe33945c87c03c600816313f749b
JHenriqueN <jhnhenriquejhn@gmail.com>
2025-06-03 16:07:18 -0300

    Primeiro commit: Projeto EcoWorld

---
+++ eco.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyEXEMPLO",
+  authDomain: "ecoworld.firebaseapp.com",
+  projectId: "ecoworld",
+  storageBucket: "ecoworld.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcde123456"
+};
